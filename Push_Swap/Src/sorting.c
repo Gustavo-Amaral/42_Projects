ff89ed74a8210b3f899953a2389e6fab2e580352
Gustavo Dorneles Do Amaral <gamaral@student.42porto.com>
2023-11-05 21:10:30 +0000

    Push_swap, developing sorting code

--- Push_Swap/Src/sorting.c
+++ Push_Swap/Src/sorting.c
@@ -6,7 +6,7 @@
 /*   By: gamaral <gamaral@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 17:44:48 by gamaral           #+#    #+#             */
-/*   Updated: 2023/11/05 20:43:45 by gamaral          ###   ########.fr       */
+/*   Updated: 2023/11/05 21:10:11 by gamaral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,15 +47,28 @@ static int	get_min_value(t_list **stack)
 void	sort_stack(t_list **stack_a, t_list **stack_b)
 {
 	t_list	*head_a;
-	int		i;
-	int		j;
+	int		max;
+	int		min;
 	int		size;
 
-	i = 0;
 	head_a = *stack_a;
 	size = ft_lstsize(head_a);
-	while ()
+	max = get_max_value(stack_a);
+	min = get_min_value(stack_a);
+	while (ft_lstsize(*stack_a) < size && !is_sorted(stack_a))
 	{
-		
+		if (ft_lstsize(*stack_a) > 3)
+		{
+			pb(stack_b, stack_a);
+			if (!is_sorted(stack_b))
+			{
+				if (ft_lstsize(*stack_b) == 2)
+					sb(stack_b);
+				else
+				{
+					
+				}
+			}
+		}
 	}
 }
