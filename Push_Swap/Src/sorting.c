a86ed1c6243e9b33be4eb4b371162b801a83b15e
Gustavo Dorneles Do Amaral <gamaral@student.42porto.com>
2023-11-05 20:45:27 +0000

    Push_swap, developing sorting code

--- Push_Swap/Src/sorting.c
+++ Push_Swap/Src/sorting.c
@@ -6,13 +6,56 @@
 /*   By: gamaral <gamaral@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 17:44:48 by gamaral           #+#    #+#             */
-/*   Updated: 2023/11/05 16:01:32 by gamaral          ###   ########.fr       */
+/*   Updated: 2023/11/05 20:43:45 by gamaral          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Includes/Push_Swap.h"
 
-/*void	sort_stack(t_list **stack_a, t_list **stack_b)
+static int	get_max_value(t_list **stack)
 {
+	t_list	*head;
+	int		max;
 
-}*/
+	head = *stack;
+	max = *(int *)head->content;
+	while (head)
+	{
+		if (*(int *)head->content > max)
+			max = *(int *)head->content;
+		head = head->next;
+	}
+	return (max);
+}
+
+static int	get_min_value(t_list **stack)
+{
+	t_list	*head;
+	int		min;
+
+	head = *stack;
+	min = *(int *)head->content;
+	while (head)
+	{
+		if (*(int *)head->content < min)
+			min = *(int *)head->content;
+		head = head->next;
+	}
+	return (min);
+}
+
+void	sort_stack(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*head_a;
+	int		i;
+	int		j;
+	int		size;
+
+	i = 0;
+	head_a = *stack_a;
+	size = ft_lstsize(head_a);
+	while ()
+	{
+		
+	}
+}
